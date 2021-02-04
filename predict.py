9947e79f53761c7d719ab3ec87a16ed2a148fb6d
Sascha Gerloff <sascha.gerloff@t-online.de>
2021-02-04 14:47:42 +0100

    Added plotting the predictions

--- predict.py
+++ predict.py
@@ -2,6 +2,8 @@ import argparse
 from clean_database import DataCleaner
 import joblib
 from metric import metric
+import pandas as pd
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Predict sales on rossman dataset')
 parser.add_argument('--data', type=str, help='Path to data set, e.g. train.csv or holdout.csv')
@@ -19,5 +21,21 @@ print("Loading model...")
 pipe = joblib.load("model.joblib")
 
 print("Predicting sales...")
+pred = pipe.predict(data.drop('Sales', axis=1))
+print(f"Test percentage mean squared error {metric( pred, data['Sales'].values )}")
 
-print(f"Test percentage mean squared error {metric( pipe.predict(data.drop('Sales', axis=1)), data['Sales'].values )}")
+unique_stores = data.Store.unique()
+compare_df = data[["Sales"]].copy()
+compare_df["Predict"] = pred
+for u in unique_stores:
+    response = input("Do you want to see a prediction? [y/N]:")
+    if response == "y":
+        map_store = (data.Store == u)
+        compare_df[map_store].plot()
+        plt.title(f"Store ID {u}")
+        plt.show()
+    else:
+        break;
+
+
+
